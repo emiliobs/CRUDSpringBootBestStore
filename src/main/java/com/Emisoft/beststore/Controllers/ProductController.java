b12fcff8b2a35dfdc41ef82d8cad2fc6673e2966
Emilio Barrera <barrera_emilio@yahoo.es>
2024-05-01 19:50:48 +0100

    Finished Project

--- src/main/java/com/Emisoft/beststore/Controllers/ProductController.java
+++ src/main/java/com/Emisoft/beststore/Controllers/ProductController.java
@@ -23,38 +23,57 @@ import java.nio.file.StandardCopyOption;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Controller handling operations related to products.
+ */
 @Controller
 @RequestMapping("/products")
-public class ProductController
-{
-   @Autowired private ProductService productService;
+public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
+    /**
+     * Show the list of products.
+     *
+     * @param model the model to be populated with data
+     * @return the view for displaying the product list
+     */
     @GetMapping({"", "/"})
-    public String showProductList(Model model )
-    {
+    public String showProductList(Model model) {
         List<Product> products = productService.findAll(Sort.by(Sort.Direction.ASC, "id"));
         model.addAttribute("products", products);
         return "products/index";
     }
 
+    /**
+     * Show the form to create a new product.
+     *
+     * @param model the model to be populated with data
+     * @return the view for creating a new product
+     */
     @GetMapping("/create")
-    public  String showCreatePage(Model model)
-    {
+    public String showCreatePage(Model model) {
         ProductDto productDto = new ProductDto();
         model.addAttribute("productDto", productDto);
         return "products/CreateProduct";
     }
 
+    /**
+     * Create a new product.
+     *
+     * @param productDto the DTO representing the product data
+     * @param result     the binding result for validation errors
+     * @return the view to redirect to after creating the product
+     */
     @PostMapping("/create")
-    public String createProduct(@Valid @ModelAttribute("productDto") ProductDto productDto, BindingResult result)
-    {
-        if (productDto.getImageFile().isEmpty())
-        {
-          result.addError(new FieldError("productDto", "imageFile", "The image file is required."));
+    public String createProduct(@Valid @ModelAttribute("productDto") ProductDto productDto, BindingResult result) {
+        // Validate image file
+        if (productDto.getImageFile().isEmpty()) {
+            result.addError(new FieldError("productDto", "imageFile", "The image file is required."));
         }
 
-        if (result.hasErrors())
-        {
+        if (result.hasErrors()) {
             return "products/CreateProduct";
         }
 
@@ -62,48 +81,47 @@ public class ProductController
         MultipartFile image = productDto.getImageFile();
         Date createAt = new Date();
         String storageFileName = createAt.getTime() + "_" + image.getOriginalFilename();
-         try
-         {
-             String uploadDir = "public/images/";
-             Path uploadPath = Paths.get(uploadDir);
+        try {
+            String uploadDir = "public/images/";
+            Path uploadPath = Paths.get(uploadDir);
 
-             if (!Files.exists(uploadPath))
-             {
-                 Files.createDirectories(uploadPath);
-             }
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
 
-             try(InputStream inputStream = image.getInputStream())
-             {
-                 Files.copy(inputStream, Paths.get(uploadDir+ storageFileName), StandardCopyOption.REPLACE_EXISTING);
-             }
+            try (InputStream inputStream = image.getInputStream()) {
+                Files.copy(inputStream, Paths.get(uploadDir + storageFileName), StandardCopyOption.REPLACE_EXISTING);
+            }
 
-         }
-         catch (Exception ex)
-         {
-             System.out.println("Exception: " + ex.getMessage());
-         }
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
 
+        // Create and save the product
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setBrand(productDto.getBrand());
+        product.setCategory(productDto.getCategory());
+        product.setPrice(productDto.getPrice());
+        product.setDescription(productDto.getDescription());
+        product.setCreatedAt(createAt);
+        product.setImageFileName(storageFileName);
 
-         Product product = new Product();
-         product.setName(productDto.getName());
-         product.setBrand(productDto.getBrand());
-         product.setCategory(productDto.getCategory());
-         product.setPrice(productDto.getPrice());
-         product.setDescription(productDto.getDescription());
-         product.setCreatedAt(createAt);
-         product.setImageFileName(storageFileName);
-
-         //Save all data of a new product include the picture
-         productService.save(product);
+        productService.save(product);
 
         return "redirect:/products";
     }
 
+    /**
+     * Show the form to edit a product.
+     *
+     * @param model the model to be populated with data
+     * @param id    the id of the product to edit
+     * @return the view for editing the product
+     */
     @GetMapping("/edit")
-    public String showEditPage(Model model, @RequestParam long id)
-    {
-        try
-        {
+    public String showEditPage(Model model, @RequestParam long id) {
+        try {
             Product product = productService.findById(id).get();
             model.addAttribute("product", product);
 
@@ -116,10 +134,7 @@ public class ProductController
 
             model.addAttribute("productDto", productDto);
 
-
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             System.out.printf("Exception: " + ex.getMessage());
             return "redirect:/products";
         }
@@ -127,94 +142,92 @@ public class ProductController
         return "products/EditProduct";
     }
 
+    /**
+     * Update a product.
+     *
+     * @param model      the model to be populated with data
+     * @param id         the id of the product to update
+     * @param productDto the DTO representing the updated product data
+     * @param result     the binding result for validation errors
+     * @return the view to redirect to after updating the product
+     */
     @PostMapping("/edit")
-    public String updateProduct(Model model, @RequestParam long id,@Valid @ModelAttribute ProductDto productDto, BindingResult result)
-    {
-        try
-        {
-           Product product = productService.findById(id).get();
-           model.addAttribute("product", product);
+    public String updateProduct(Model model, @RequestParam long id, @Valid @ModelAttribute ProductDto productDto,
+            BindingResult result) {
+        try {
+            Product product = productService.findById(id).get();
+            model.addAttribute("product", product);
 
-           if (result.hasErrors())
-           {
-               return "products/EditProduct";
-           }
+            if (result.hasErrors()) {
+                return "products/EditProduct";
+            }
 
-           if (!productDto.getImageFile().isEmpty())
-           {
-               //delete old image
-               String uploadDir = "public/images/";
-               Path oldImagePath = Paths.get(uploadDir + product.getImageFileName());
+            if (!productDto.getImageFile().isEmpty()) {
+                // Delete old image
+                String uploadDir = "public/images/";
+                Path oldImagePath = Paths.get(uploadDir + product.getImageFileName());
 
-               try
-               {
-                   Files.delete(oldImagePath);
-               }
-               catch (Exception ex)
-               {
-                   System.out.println("Exception: " + ex.getMessage());
-               }
+                try {
+                    Files.delete(oldImagePath);
+                } catch (Exception ex) {
+                    System.out.println("Exception: " + ex.getMessage());
+                }
 
-               // Save new Image file
-               MultipartFile image = productDto.getImageFile();
-               Date createdAt = new Date();
-               String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+                // Save new image file
+                MultipartFile image = productDto.getImageFile();
+                Date createdAt = new Date();
+                String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
 
-               try(InputStream inputStream = image.getInputStream())
-               {
-                   Files.copy(inputStream, Paths.get(uploadDir + storageFileName), StandardCopyOption.REPLACE_EXISTING);
-               }
+                try (InputStream inputStream = image.getInputStream()) {
+                    Files.copy(inputStream, Paths.get(uploadDir + storageFileName), StandardCopyOption.REPLACE_EXISTING);
+                }
 
-               product.setImageFileName(storageFileName);
+                product.setImageFileName(storageFileName);
+            }
 
-           }
+            // Update product data
+            product.setName(productDto.getName());
+            product.setBrand(productDto.getBrand());
+            product.setCategory(productDto.getCategory());
+            product.setPrice(productDto.getPrice());
+            product.setDescription(productDto.getDescription());
 
-           product.setName(productDto.getName());
-           product.setBrand(productDto.getBrand());
-           product.setCategory(productDto.getCategory());
-           product.setPrice(productDto.getPrice());
-           product.setDescription(productDto.getDescription());
+            productService.save(product);
 
-           productService.save(product);
-
-
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             System.out.println("Exception: " + ex.getMessage());
         }
 
         return "redirect:/products";
     }
 
+    /**
+     * Delete a product.
+     *
+     * @param id the id of the product to delete
+     * @return the view to redirect to after deleting the product
+     */
     @GetMapping("/delete")
-    public String deleteProduct(@RequestParam long id)
-    {
-        try
-        {
-           Product product = productService.findById(id).get();
+    public String deleteProduct(@RequestParam long id) {
+        try {
+            Product product = productService.findById(id).get();
 
-           // Delete product image
+            // Delete product image
             Path imagePath = Paths.get("public/images" + product.getImageFileName());
 
-            try
-            {
+            try {
                 Files.delete(imagePath);
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 System.out.println("Exception: " + ex.getMessage());
             }
 
-            // Delete the product from the database:
+            // Delete the product from the database
             productService.delete(product);
 
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             System.out.println("Exception: " + ex.getMessage());
         }
 
-        return  "redirect:/products";
+        return "redirect:/products";
     }
 }
