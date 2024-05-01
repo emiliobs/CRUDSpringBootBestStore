2136fea17124ac4a6603a97c7c866f0a6b2271ba
Emilio Barrera <barrera_emilio@yahoo.es>
2024-05-01 16:32:36 +0100

    form and capture error done

--- src/main/java/com/Emisoft/beststore/Controllers/ProductController.java
+++ src/main/java/com/Emisoft/beststore/Controllers/ProductController.java
@@ -1,11 +1,18 @@
 package com.Emisoft.beststore.Controllers;
 
 import com.Emisoft.beststore.Models.Product;
+import com.Emisoft.beststore.Models.ProductDto;
 import com.Emisoft.beststore.Services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,8 +26,32 @@ public class ProductController
     @GetMapping({"", "/"})
     public String showProductList(Model model )
     {
-        List<Product> products = productService.findAll();
+        List<Product> products = productService.findAll(Sort.by(Sort.Direction.ASC, "id"));
         model.addAttribute("products", products);
         return "products/index";
+    }
+
+    @GetMapping("/create")
+    public  String showCreatePage(Model model)
+    {
+        ProductDto productDto = new ProductDto();
+        model.addAttribute("productDto", productDto);
+        return "products/CreateProduct";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(@Valid @ModelAttribute("productDto") ProductDto productDto, BindingResult result)
+    {
+        if (productDto.getImageFile().isEmpty())
+        {
+          result.addError(new FieldError("productDto", "imageFile", "The image file is required."));
+        }
+
+        if (result.hasErrors())
+        {
+            return "products/CreateProduct";
+        }
+
+        return "redirect:/products";
     }
 }
