2b13db345801664ce645b141320505ee351c5ed9
Emilio Barrera <barrera_emilio@yahoo.es>
2024-05-01 16:57:57 +0100

    created prodcut with picture done

--- src/main/java/com/Emisoft/beststore/Controllers/ProductController.java
+++ src/main/java/com/Emisoft/beststore/Controllers/ProductController.java
@@ -14,7 +14,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -51,6 +59,44 @@ public class ProductController
         {
             return "products/CreateProduct";
         }
+
+        // Save image file
+        MultipartFile image = productDto.getImageFile();
+        Date createAt = new Date();
+        String storageFileName = createAt.getTime() + "_" + image.getOriginalFilename();
+         try
+         {
+             String uploadDir = "public/images/";
+             Path uploadPath = Paths.get(uploadDir);
+
+             if (!Files.exists(uploadPath))
+             {
+                 Files.createDirectories(uploadPath);
+             }
+
+             try(InputStream inputStream = image.getInputStream())
+             {
+                 Files.copy(inputStream, Paths.get(uploadDir+ storageFileName), StandardCopyOption.REPLACE_EXISTING);
+             }
+
+         }
+         catch (Exception ex)
+         {
+             System.out.println("Exception: " + ex.getMessage());
+         }
+
+
+         Product product = new Product();
+         product.setName(productDto.getName());
+         product.setBrand(productDto.getBrand());
+         product.setCategory(productDto.getCategory());
+         product.setPrice(productDto.getPrice());
+         product.setDescription(productDto.getDescription());
+         product.setCreatedAt(createAt);
+         product.setImageFileName(storageFileName);
+
+         //Save all data of a new product include the picture
+         productService.save(product);
 
         return "redirect:/products";
     }
